1a6296d87a0cdc88b2c3112bd2c151721111344c
unknown <yamilarobles10@gmail.com>
2024-01-09 17:55:58 +0100

    Initial commit

---
+++ kata.js
@@ -0,0 +1,51 @@
+
+
+//KATA 1: Muestre un “¡Hola Mundo!” por consola
+
+/*function mostrarSaludo (){
+    console.log("Hola mundo");
+}*/
+
+/*mostrarSaludo();*/
+
+//KATA 2: Lea tu nombre y devuelva un “¡Hola tuNombre!”
+
+/*function leerNombre(){
+    let nombre= "Claudia";
+    console.log("hola " + nombre)
+}
+leerNombre();*/
+
+//KATA 3 Lea un número y devuelva el doble
+
+/*function readNumber (){
+    let numero= 10;
+    return console.log(numero * 2);
+}
+readNumber();*/
+
+// KATA 4 : Lea un número y devuelva la mitad
+
+/*function readNumber (){
+    let numero= 10;
+    return console.log(numero / 2);
+}
+readNumber();*/
+
+// KATA 5 : Lea dos números y devuelva el mayor
+
+/*function returnMaxNum(){
+    let numero1  = 10;
+    let numero2 = 25;
+    if (numero1 >= numero2){
+        return console.log("El numero mayor es: " + numero1)
+    }
+    else{
+        console.log("El mayor es: " + numero2)
+    }
+}
+returnMaxNum();*/
+
+//KATA 6 :
+
+
